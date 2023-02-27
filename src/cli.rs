eae5417152826ec25f0b9e6cab322a0211ae5787
Chris Renfrow <dev@chrisrenfrow.me>
2023-02-27 13:36:14 -0800

    Remove default config path doc-test
    Fixes #2

--- src/cli.rs
+++ src/cli.rs
@@ -4,8 +4,5 @@ use clap::Parser;
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
     /// Path to alternate config (default is `~/.config/mystical-sphere/config.toml`)
-    /// ```
-    /// assert_eq!(mystical_sphere::DEFAULT_CONFIG_PATH, "~/.config/mystical-sphere/config.toml");
-    /// ```
     pub config: Option<String>,
 }
